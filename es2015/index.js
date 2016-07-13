6329a3a4e5b02379c3b31ff24abf9a32fde056a6
IliaVolk <liailia119@gmail.com>
2016-07-13 23:57:18 +0300

    es2015

---
+++ es2015/index.js
@@ -0,0 +1,133 @@
+/**
+ * Created by user on 13.07.2016.
+ */
+"use strict";
+
+class Fighter {
+    /**
+     * @param {string} name
+     * @param {number} power
+     * @param {number} health
+     */
+    constructor(name, power, health) {
+        this.name = name;
+        this.power = power;
+        this.health = health;
+        this.maxHealth = health;
+    }
+
+    /**
+     * @param {number} damage
+     * @return {Fighter} this
+     */
+    setDamage(damage) {
+        this.health -= damage;
+        return this;
+    }
+
+    /**
+     * @param {number} point
+     * @param {Fighter} enemy
+     * @return {Fighter} this
+     */
+    hit(enemy, point) {
+        let damage = point * this.power;
+        enemy.setDamage(damage);
+        return this;
+    }
+
+    healthDesc() {
+        return `${this.health}/${this.maxHealth}`;
+    }
+}
+/**
+ * @class ImprovedFighter extends Fighter
+ */
+class ImprovedFighter extends Fighter {
+    /**
+     * @param {string} name
+     * @param {number} power
+     * @param {number} health
+     */
+    constructor(name, power, health) {
+        super(name, power, health);
+    }
+
+    /**
+     * @param {Fighter} enemy
+     * @param {number} point
+     * @return {ImprovedFighter} this
+     */
+    doubleHit(enemy, point) {
+        super.hit(enemy, point * 2);
+        return this;
+    }
+}
+class GotWinnerException extends Error {
+    /**
+     *
+     * @param {Fighter} winner
+     * @param {Fighter} looser
+     */
+    constructor(winner, looser) {
+        super();
+        this.winner = winner;
+        this.looser = looser;
+    }
+
+    printResult() {
+        console.log(
+            `Fight results:\n` +
+            `   Winner is ${this.winner.name} (${this.winner.healthDesc()})\n` +
+            `   Looser is ${this.looser.name} (${this.looser.healthDesc()})`);
+    }
+}
+/**
+ * @param {Fighter} source
+ * @param {Fighter} target
+ * @param {number} point
+ */
+const doHit = (source, target, point) => {
+    console.log(`${source.name} (${source.healthDesc()}) ` +
+        `hits ${target.name} (${target.healthDesc()}) ` +
+        `on point ${point}`);
+    source.hit(target, point);
+    console.log(`${target.name}'s health is now ${target.healthDesc()}`);
+    if (target.health <= 0) {
+        throw new GotWinnerException(source, target);
+    }
+};
+/**
+ * @param {Fighter} fighter
+ * @param {ImprovedFighter|Fighter} improvedFighter
+ * @param {Array<number>} points
+ */
+var fight = (fighter, improvedFighter, ...points) => {
+    console.log(`Begins battle between ${fighter.name} and ${improvedFighter.name}`);
+    let doBattle = () => {
+        for (var i = 0; i < points.length; i += 2) {
+            doHit(fighter, improvedFighter, points[i]);
+            if (i + 1 < points.length)
+                doHit(improvedFighter, fighter, points[i + 1]);
+        }
+    };
+    try {
+        doBattle();
+        console.log("No winner!")
+    } catch (e) {
+        if (e instanceof GotWinnerException)
+            e.printResult();
+        else throw e;
+    } finally {
+        console.log("Fight ends")
+    }
+};
+
+var fighter = new Fighter("fighter", 2, 100);
+
+var improvedFighter = new ImprovedFighter("improvedFighter", 3, 100);
+fight(fighter, improvedFighter, 25, 13, 45, 20, 10, 25);
+
+var fighter2 = new Fighter("fighter2", 3, 200);
+var fighter3 = new Fighter("fighter3", 4, 300);
+fight(fighter2, fighter3, 25, 31, 1);
